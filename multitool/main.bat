05f9b6a52ca4fb57aee46f214bcf3b15cab29605
Jirmon1 <rickjameeh@gmail.com>
2025-07-27 15:15:33 +1000

    Update main.bat

--- multitool/main.bat
+++ multitool/main.bat
@@ -1,38 +1,65 @@
-@echo off
-title MultiTool - by   PVXN
-chcp 65001 >nul
-cd files
-color 5
-:start
-call :banner
+     @echo off
+      title --------([NVJN Proprietary Mass Reporter]--------
+      echo.
+      set /p ip="Input Account Username.: "
+      set /p user="Input Report Type. WARN/TERM/LOCK: "
 
-:menu
-for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%A
-echo.
-echo.
-echo [38;2;255;255;0m        ╔═(1) TERM 2[0m  
-echo [38;2;255;255;0m        ║[0m  
-echo [38;2;255;255;0m        ╠══(2) LOCK[0m  
-echo [38;2;255;255;0m        ║[0m  
-echo [38;2;255;255;0m        ╠═══(3) UNTERM[0m  
-echo [38;2;255;255;0m        ║[0m  
-echo [38;2;255;255;0m        ╚╦═══(4) UNLOCK[0m  
-echo [38;2;255;255;0m         ║[0m  
-set /p input=.%BS% [38;2;255;255;0m        ╚══════^>[0m  
-if /I %input% EQU 1 start ph2.lnk
-if /I %input% EQU 2 start Wireshark.lnk
-if /I %input% EQU 3 start vbox.lnk
-if /I %input% EQU 4 start putty.exe
-cls
-goto start
 
-:banner
-echo.
-echo.
-echo                     [38;2;255;0;0m███╗   ███╗██╗   ██╗██╗  ████████╗██╗    ████████╗ ██████╗  ██████╗ ██╗[0m     
-echo                     [38;2;255;51;0m████╗ ████║██║   ██║██║  ╚══██╔══╝██║    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     [0m
-echo                     [38;2;255;102;0m██╔████╔██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║    [0m 
-echo                     [38;2;255;153;0m██║╚██╔╝██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║     [0m
-echo                     [38;2;255;204;0m██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║       ██║   ╚██████╔╝╚██████╔╝███████╗[0m
-echo                     [38;2;255;255;0m╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝[0m
-echo.
+      set /a count=1
+
+
+
+
+      echo Moving Network Traffic to Mullvad Servers.
+	  
+	  
+	  
+      pause
+
+
+
+
+
+      echo JS Requests Sent. Loading ROBLOX Cookies.
+	  
+	  
+	  
+      pause
+
+
+
+
+
+      echo Executing XML to Bypass Report Cooldowns. Mass Report Hivenet Status: Online
+	  
+	  
+	  
+      pause
+
+
+
+
+
+      echo Account sorte30 Mass Reported.
+	  
+      echo Chance Of Termination: 70%
+	  
+      echo Refresh Page Of sorte30 In 24 hours For Results.
+
+
+pause
+
+
+
+
+
+echo 
+
+
+
+
+
+
+
+
+pause
